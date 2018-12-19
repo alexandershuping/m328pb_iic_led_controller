633fbceb18d8e6b3f8fef708a492821ae63e0bcd
Alexander Shuping <alexandershuping@gmail.com>
2018-12-18 19:16:57 -0500

    Add explanation for the large synchronization command

--- rainbow_blast.py
+++ rainbow_blast.py
@@ -17,7 +17,21 @@ SER = serial.Serial('/dev/ttyUSB0', 38400, parity=serial.PARITY_NONE)
 while True:
     for i in range(0, 500):
         main()
-    # SER.write(b'xt001')
-    # time.sleep(3)
+
+    # Explanation:
+    #   x       - clear any previous command that might have been left
+    #             unfinished
+    #   t 00    - select general-call
+    #   1       - put all devices into single-color (i.e. frozen) mode
+    #   i 30 40 - include device 0x30 and indicate that it should have
+    #             a phase offset of 0x40 (0x40 / 0xff == 25%)
+    #   i 31 c0 - include device 0x31 and indicate that it should have
+    #             a phase offset of 0xc0 (0xc0 / 0xff == 75%)
+    #   S       - perform an exclusive-synchronize. 0x30 will go to 25%
+    #             offset, 0x31 will go to 75% offset, any other devices
+    #             will be unaffected
+    #   2       - put all devices into sine-wave mode.
+    #
+    # 0x30 and 0x31 are now synchronized, with 0x31 leading 0x30 by 50%
     SER.write(b'xt001i3040i31c0S2')
     time.sleep(5)
